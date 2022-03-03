f7975f4cf1277954c4cd25dc6651b9fdeec07240
izevbaye <ehi.baye@gmail.com>
2022-03-03 13:46:44 +0000

    demo
    demo

---
+++ CoreWithWebAPI/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWithWebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("status/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetStatus([FromRoute] string id)
+        {
+            return Ok(id);
+        }
+    }
+
+
+   
+}
